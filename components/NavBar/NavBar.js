a72ed46ee6c63d95c639ca8e754ee9848bac5ef5
Alberto <albertosolpal@gmail.com>
2020-12-15 21:55:20 +0100

    Crated main page mobile

--- components/NavBar/NavBar.js
+++ components/NavBar/NavBar.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types'
 
 import { useRouter } from 'next/router'
 
+import useMediaQuery from '@material-ui/core/useMediaQuery'
 import { makeStyles } from '@material-ui/core/styles'
 import { Typography } from '@material-ui/core'
 import Menu from '../Menu'
@@ -16,22 +17,31 @@ const useStyles = makeStyles(theme => ({
     display: 'flex',
     paddingLeft: '140px',
     paddingRight: '115px',
-    marginTop: '1%'
+    marginTop: '1%',
+    [theme.breakpoints.down('sm')]: {
+      padding: '3%'
+    }
   },
   logo: {
     fontWeight: 'bold',
-    cursor: 'pointer'
+    cursor: 'pointer',
+    [theme.breakpoints.down('sm')]: {
+      fontSize: '2.5rem'
+    }
   }
 }))
 
 const NavBar = ({ handleAnimation }) => {
   const classes = useStyles()
   const router = useRouter()
+  const mobile = useMediaQuery(theme => theme.breakpoints.down('md'))
 
   return (
         <nav className={classes.root}>
           <Typography onClick={() => handleClick('/', handleAnimation, router)} variant="h2" className={classes.logo}>APBEB</Typography>
-          <Menu handleAnimation={handleAnimation} />
+          {
+            !mobile && <Menu handleAnimation={handleAnimation} />
+          }
         </nav>
   )
 }
--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types'
 
 import { withTranslation } from '../i18n'
 
+import useMediaQuery from '@material-ui/core/useMediaQuery'
 import { makeStyles } from '@material-ui/core/styles'
 import { Typography } from '@material-ui/core'
 import Social from '../components/Social/Social'
@@ -11,15 +12,21 @@ const useStyles = makeStyles(theme => ({
   root: {
     height: '100%',
     width: '100%',
-    display: 'flex'
+    display: 'flex',
+    [theme.breakpoints.down('sm')]: {
+      flexDirection: 'column'
+    }
   },
   homeLeft: {
     display: 'flex',
     flex: 1,
     flexDirection: 'column',
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
     alignItems: 'flex-start',
-    backgroundColor: theme.palette.primary.main
+    backgroundColor: theme.palette.primary.main,
+    [theme.breakpoints.down('sm')]: {
+      order: 1
+    }
   },
   title: {
     display: 'flex',
@@ -30,31 +37,47 @@ const useStyles = makeStyles(theme => ({
     fontSize: '4.5rem',
     fontWeight: '400',
     paddingLeft: '140px',
-    marginTop: '5rem'
+    marginTop: '5rem',
+    [theme.breakpoints.down('sm')]: {
+      fontSize: '3rem',
+      paddingLeft: '2rem',
+      width: '50%'
+    }
   },
   homeRight: {
     display: 'flex',
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    background: theme.palette.secondary.main
+    background: theme.palette.secondary.main,
+    [theme.breakpoints.down('sm')]: {
+      alignItems: 'flex-end',
+      order: 0
+    }
   },
   homeImage: {
     width: '700px',
-    objectFit: 'contain'
+    objectFit: 'contain',
+    [theme.breakpoints.down('sm')]: {
+      width: '80%'
+    }
   }
 }))
 
 const Home = ({ t }) => {
   const classes = useStyles()
+  const mobile = useMediaQuery(theme => theme.breakpoints.down('md'))
+
   return (
     <div className={classes.root}>
       <div className={classes.homeLeft}>
         <Typography variant="h1" className={classes.title}>{t('home.title')}</Typography>
-        <Social footer={true}/>
+        {
+          !mobile && <Social footer={true}/>
+        }
       </div>
       <div className={classes.homeRight}>
-        <img src="static/images/zapato.png" alt="wedding" className={classes.homeImage}/>
+        <img src="static/images/shoe.png" alt="wedding" className={classes.homeImage}/>
       </div>
     </div>
   )
