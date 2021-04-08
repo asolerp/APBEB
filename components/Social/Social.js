e269b7394b79b4b61ee2dd23f54b46d6ebd69481
Alberto Soler Palau <albertosolpal@gmail.com>
2021-04-08 09:33:25 +0200

    fixes social styles

--- components/Social/Social.js
+++ components/Social/Social.js
@@ -1,48 +1,49 @@
 import React from 'react'
 import { makeStyles } from '@material-ui/core/styles'
 
-const useStyles = makeStyles(theme => ({
-  container: {
-    display: 'flex',
-    flexGrow: 1,
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    marginTop: '3%',
-    marginBottom: '3%',
-    [theme.breakpoints.down('sm')]: {
-      marginTop: '10%',
-      marginBottom: '10%',
-      padding: 0,
-      width: '50%'
-    }
-  },
-  socialIcon: {
-    width: '40px'
-  },
-  socialFooter: {
-    border: '1px solid black',
-    padding: '5%',
-    borderRadius: '5px'
-  },
-  socialIconLinkedin: {
-    width: '35px'
-  },
-  socialIconLinkedinFooter: {
-    border: '1px solid black',
-    padding: '8%',
-    borderRadius: '5px'
-  },
-  liStyle: {
-    listStyle: 'none'
-  }
-
-}))
-
 const Social = ({ footer, mainPage }) => {
+  const useStyles = makeStyles(theme => ({
+    container: {
+      display: 'flex',
+      flexGrow: 1,
+      width: mainPage ? 'auto' : '10%',
+      padding: !mainPage && '0',
+      justifyContent: 'space-between',
+      alignItems: 'center',
+      marginTop: '3%',
+      marginBottom: '3%',
+      [theme.breakpoints.down('sm')]: {
+        marginTop: '10%',
+        marginBottom: '10%',
+        padding: 0,
+        width: '50%'
+      }
+    },
+    socialIcon: {
+      width: '40px'
+    },
+    socialFooter: {
+      border: '1px solid black',
+      padding: '5%',
+      borderRadius: '5px'
+    },
+    socialIconLinkedin: {
+      width: '35px'
+    },
+    socialIconLinkedinFooter: {
+      border: '1px solid black',
+      padding: '8%',
+      borderRadius: '5px'
+    },
+    liStyle: {
+      listStyle: 'none'
+    }
+
+  }))
+
   const classes = useStyles()
   return (
     <ul
-    style={{ width: mainPage ? 'auto' : '10%', padding: !mainPage && '0' }}
     className={classes.container}>
       <li className={classes.liStyle}>
         <a href="https://instagram.com/apbeb">
