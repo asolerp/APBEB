7f7f1511db81be0695641089dea3f8d5592060f0
Alberto Soler Palau <albertosolpal@gmail.com>
2021-04-16 09:40:46 +0200

    check politica privacidad

--- components/Form/Form.js
+++ components/Form/Form.js
@@ -1,12 +1,15 @@
 import React, { useState } from 'react'
+import { withStyles, makeStyles } from '@material-ui/core/styles'
 import { useForm } from 'react-hook-form'
 
-import { makeStyles } from '@material-ui/core/styles'
 import CircularProgress from '@material-ui/core/CircularProgress'
+import Checkbox from '@material-ui/core/Checkbox'
 
 import { init, sendForm } from 'emailjs-com'
 
 import Input from '../Input'
+import clsx from 'clsx'
+import { FormControlLabel } from '@material-ui/core'
 init('user_Qb7VouW29dT11OD4oNWHe')
 
 const useStyles = makeStyles(theme => ({
@@ -30,13 +33,28 @@ const useStyles = makeStyles(theme => ({
     padding: '1rem',
     fontSize: '1.1rem',
     fontWeight: '700',
+    marginTop: theme.spacing(2),
     border: 0
+  },
+  disabled: {
+    opacity: 0.2
   }
 }))
+
+const CustomCheckbox = withStyles((theme) => ({
+  root: {
+    color: theme.palette.info.main,
+    '&$checked': {
+      color: theme.palette.info.main
+    }
+  },
+  checked: {}
+}))((props) => <Checkbox color="default" {...props} />)
 
 const Form = () => {
   const classes = useStyles()
   const [loading, setLoading] = useState(false)
+  const [aceptaPoliticas, setAceptaPoliticas] = useState(false)
 
   const { register, handleSubmit, errors, reset } = useForm()
   const onSubmit = async (data) => {
@@ -49,6 +67,10 @@ const Form = () => {
       reset()
       setLoading(false)
     }
+  }
+
+  const handleInputChange = (e) => {
+    setAceptaPoliticas(e.target.checked)
   }
 
   return (
@@ -65,10 +87,31 @@ const Form = () => {
        <Input label="Email" name="email" register={register} required />
        <Input label="Asunto" name="subject" register={register} required />
        <Input type="textarea" label="Mensaje" name="message" register={register} required />
+      <FormControlLabel
+          control={
+            <CustomCheckbox
+            checked={aceptaPoliticas}
+            onChange={handleInputChange}
+            name="checkedB"
+            color="primary"
+          />
+          }
+          label="He leído y acepto la política de privacidad"
+        />
+       {/* <label>
+         <input
+          type="checkbox"
+          id="cbox1"
+          checked={aceptaPoliticas}
+          onChange={handleInputChange}
+          >
+            He leído y acepto la política de privacidad
+         </input>
+        </label> */}
       </div>
       {errors.exampleRequired && <span>This field is required</span>}
       {
-        loading ? <CircularProgress color="secondary" /> : <input type="submit" className={classes.submit} value={'Enviar'}/>
+        loading ? <CircularProgress color="secondary" /> : <input disabled={!aceptaPoliticas} type="submit" className={clsx(classes.submit, { [classes.disabled]: !aceptaPoliticas })} value={'Enviar'}/>
       }
 
     </form>
